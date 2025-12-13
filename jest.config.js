e29e40a971d5b5938c50efe4b1c4916b0821cc5b
ysaacnoa <aysaac.correa@gmail.com>
2025-12-13 00:14:52 -0500

    feat(jest): 🔧 configure jest test for project

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|@react-navigation|react-native-hooks)/)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest/setup.js'],
+  moduleNameMapper: {
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+    '^react-native-hooks$': '<rootDir>/__mocks__/react-native-hooks.tsx',
+  },
+  transform: {
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
