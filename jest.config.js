22e7a127dc5a250ce0718d7accfcb39eaceb4f10
ysaacnoa <aysaac.correa@gmail.com>
2025-12-14 11:02:48 -0500

    feat(jest): 🔧 add testing config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!(@react-native|react-native|@react-navigation|react-native-hooks)/)',
+    'node_modules/(?!(@react-native|react-native|@react-navigation|react-native-hooks|@react-native-vector-icons)/)',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest/setup.js'],
   moduleNameMapper: {
     '^@modules/(.*)$': '<rootDir>/src/modules/$1',
     '^react-native-hooks$': '<rootDir>/__mocks__/react-native-hooks.tsx',
+    '\\.(ttf)$': '<rootDir>/__mocks__/file-mock.js',
+    '^@react-native-vector-icons/common$': '<rootDir>/__mocks__/@react-native-vector-icons/common.js',
+    '^@react-native-vector-icons/ionicons$': '<rootDir>/__mocks__/@react-native-vector-icons/ionicons.js',
   },
   transform: {
     '^.+\\.(js|ts|tsx)$': 'babel-jest',
